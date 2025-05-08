1540ef4ccd66831cff0afc45a2eefac512b9739f
Dai Palacios <daianapalacios213@gmail.com>
2025-05-08 13:52:32 +0000

    web2

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type {NextConfig} from 'next';
 
-// This will be set by the GitHub Actions workflow
-// It extracts 'repo-name' from 'owner/repo-name'
-const repoName = process.env.GITHUB_REPOSITORY_NAME || '';
+const repoName = process.env.GITHUB_REPOSITORY_NAME || 'Pasantias';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // Set basePath and assetPrefix only for production builds on GitHub Pages
-  // This ensures local development (npm run dev) works without the basePath
   basePath: process.env.GITHUB_PAGES_DEPLOY === 'true' ? `/${repoName}` : '',
   assetPrefix: process.env.GITHUB_PAGES_DEPLOY === 'true' ? `/${repoName}/` : '',
   typescript: {
